e0bdab11450c36f269139242b21b5c7c05155e93
jacktown11 <jikai_tang@163.com>
2017-07-05 16:36:48 +0800

    portfolio

--- Build_a_Personal_Portfolio_Webpage0/js1.js
+++ Build_a_Personal_Portfolio_Webpage0/js1.js
@@ -1,15 +1,17 @@
 window.onload = function(){
-  changeLabelPos();
+  changeLabel();
   dynamicNav();
   test();
 };
-function changeLabelPos(){
+function changeLabel(){
+  //change label's position, color and visibility in form
   var form1 = document.getElementById("form"),
       labelsOld = document.getElementsByTagName("label"),
       len = labelsOld.length,
       inputs = new Array(len),
       labels = new Array(len);
   var getOrder = function(ele){
+    //get a form field's order in the form
       if(typeof ele === "object" && typeof ele.id === "string"){
         switch(ele.id.toLowerCase()){
           case "name":
@@ -27,12 +29,15 @@ function changeLabelPos(){
       return -1;
   };
   var handleFocus = function(){
-        var j = getOrder(this);
+        var j = getOrder(this); 
+        //get the event target's order in the form
         if(j>=0){
           labels[j].style.color = "#722872";
           var thisele = this,
-              show = (labels[j].style.visibility === "visible"), //当前是否显示着label
+              show = (labels[j].style.visibility === "visible"), 
+              //判断当前是否显示着label
               checkInput = function(){
+                //check the input's state to toggle its look
                 if(thisele.value.length !== 0 
                   && thisele.value !== thisele.defaultValue){
                   //有非默认值的输入字符串
@@ -59,8 +64,10 @@ function changeLabelPos(){
       handleBlur = function(){
         var j = getOrder(this);
         if(this.value){
+          //effective value in the field now
           labels[j].style.color = "#888888";
         }else{
+          //the field is still empty so hidden its label
           labels[j].style.visibility = "hidden";
           labels[j].style.top = "2em";
           labels[j].style.color = "#722872";
@@ -78,14 +85,31 @@ function dynamicNav(){
       about = document.getElementById("about"),
       portfolio = document.getElementById("portfolio"),
       contact = document.getElementById("contact"),
-      navAbout = document.getElementById("nav-about").firstChild,
-      navPortfolio = document.getElementById("nav-portfolio").firstChild,
-      navContact = document.getElementById("nav-contact").firstChild,
-      currentPos = 0;
+      navAbout = document.getElementById("nav-about"),
+      navPortfolio = document.getElementById("nav-portfolio"),
+      navContact = document.getElementById("nav-contact"),
+      currentPos = 0,
+      portfolioTop = getOffsetTop(portfolio),
+      contactTop = getOffsetTop(contact);
   var updateActiveNav = function(activeEle){
+        //set the activeEle as "active" class
+        //and delete other ele's "active" class
         if(typeof activeEle === "object"){
           switch(activeEle){
             case navAbout:
+              addClass(activeEle,"active");
+              removeClass(navPortfolio,"active");
+              removeClass(navContact,"active");
+              break;
+            case navPortfolio:
+              addClass(activeEle,"active");
+              removeClass(navAbout,"active");
+              removeClass(navContact,"active");
+              break;
+            case navContact:
+              addClass(activeEle,"active");
+              removeClass(navPortfolio,"active");
+              removeClass(navAbout,"active");
               break;
           }
         }
@@ -98,25 +122,32 @@ function dynamicNav(){
             }else{
               event.returnValue = false;
             }
-            transiteBodyScrollTop(body,body.scrollTop,navElePos);
-            updateNavClass();
+            transiteScrollTop(body,body.scrollTop,navElePos);
+            //sroll the document the navEle's target part
+            updateActiveNav(navEle);
+            //update the nav's look
         };
+      },
+      handleScroll = function(){
+        //judge which part is in the viewport
+        //and update the navigation's look
+        if(body.scrollTop < portfolioTop){
+          updateActiveNav(about);
+        }else if(body.scrollTop < contactTop){
+          updateActiveNav(portfolio);
+        }else{
+          updateActiveNav(contact);
+        }
       };
-  var portfolioTop = getOffsetTop(portfolio),
-      contactTop = getOffsetTop(contact);
-  setNavOnclick(navAbout,0);
-  setNavOnclick(navPortfolio,portfolioTop);
+  setNavOnclick(navAbout,0),
+  setNavOnclick(navPortfolio,portfolioTop),
   setNavOnclick(navContact,contactTop);
 
   if(document.onmouseWheel){
-    document.onmouseWheel = function(){
-      updateNavClass(); 
-    };
+    document.onmouseWheel = handleScroll;
   }
   if(document.onDOMMouseScroll){
-    document.onDOMMouseScroll = function(){
-      updateNavClass();
-    };
+    document.onDOMMouseScroll = handleScroll;
   }
 
 }
@@ -131,24 +162,31 @@ function getOffsetTop(ele){
         return top;
       }
 function addClass(ele,cls){
-        if(typeof ele === "object" && typeof cls === "string"){
+        if(typeof ele === "object" 
+          && typeof cls === "string"
+          && cls.length > 0)
+        {
           var pos = -1;
-          if((pos = ele.style.className.indexOf(cls)) < 0){
-            ele.style.className += " " + cls;
+          if(!ele.className || (pos = ele.className.indexOf(cls)) < 0){
+            ele.className += " " + cls;
           }
         }
       }
 function removeClass(ele,cls){
-        if(typeof ele === "object" && typeof cls === "string"){
-          var pos = -1;
-          while((pos = ele.style.className.indexOf(cls)) >= 0){
-            ele.style.className.replace(cls,"");
-          }
-          ele.style.className.replace(/ +/g," ");
-          ele.style.className = ele.style.className.trim();
-        }        
-      }
-function transiteBodyScrollTop(ele,start,end){
+  if(typeof ele === "object" 
+    && typeof cls === "string" 
+    && cls.length > 0
+    && ele.className)
+  {
+    var pos = -1;
+    while((pos = ele.className.indexOf(cls)) >= 0){
+      ele.className = ele.className.replace(cls,"");
+    }
+    ele.className = ele.className.replace(/ +/g," ");
+    ele.className = ele.className.trim();
+  }     
+}
+function transiteScrollTop(ele,start,end){
   //change the ele's scrollTop value from start to end gradually
   if(typeof ele === "object"
     && typeof start === "number" 
@@ -170,9 +208,9 @@ function transiteBodyScrollTop(ele,start,end){
     setTimeout(update,0);
   }
 }
+
 function test(){
-  var ele = document.createElement("div");
-  ele.style.className = "this that goog time fun sssd";
-  removeClass(ele,"that");
-  console.log(ele.style.className);
+  document.body.scrollTop = 800;
+  console.log(document.body.scrollTop);
+  console.log(document.activeElement.scrollTop);
 }
